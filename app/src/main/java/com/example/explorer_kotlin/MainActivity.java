3eb9ebdb1139bac37b8e367d486456ee6b3faf82
Sujata Baral <sujata689@gmail.com>
2021-01-14 19:49:48 -0600

    text size and theme change

--- app/src/main/java/com/example/explorer_kotlin/MainActivity.java
+++ app/src/main/java/com/example/explorer_kotlin/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.explorer_kotlin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
@@ -11,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
     }
 }
